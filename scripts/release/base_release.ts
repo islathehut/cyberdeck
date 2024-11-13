3a6f42fcbe9b89d9d0b6c44f001721452ba6bc12
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-11-13 10:03:48 -0500

    Better release for windows

---
+++ scripts/release/base_release.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export const EXECUTABLE_PATH_WIN = path.join(process.cwd(), 'bin/release/cyberdeck.exe');
+export const BLOB_PATH = path.join(process.cwd(), 'bin/cyberdeck-sea.blob');
+export const NODE_SEA_FUSE = 'NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2';
---
+++ scripts/release/release_windows.ts
@@ -0,0 +1,74 @@
+import fs from 'node:fs/promises';
+import child from 'node:child_process';
+import { BLOB_PATH, EXECUTABLE_PATH_WIN, NODE_SEA_FUSE } from './base_release';
+import type { PathLike } from 'node:fs';
+
+/**
+ * Searches the installed Windows SDKs for the most recent signtool.exe version
+ * Taken from https://github.com/dlemstra/code-sign-action
+ * @returns Path to most recent signtool.exe (x86 version)
+ */
+const getSigntoolLocation = async (): Promise<string> => {
+  const windowsKitsFolder = 'C:/Program Files (x86)/Windows Kits/10/bin/';
+  const folders = await fs.readdir(windowsKitsFolder);
+  let fileName = '';
+  let maxVersion = 0;
+  for (const folder of folders) {
+    if (!folder.endsWith('.0')) {
+      continue;
+    }
+    const folderVersion = parseInt(folder.replace(/\./g, ''));
+    if (folderVersion > maxVersion) {
+      const signtoolFilename = `${windowsKitsFolder}${folder}/x64/signtool.exe`;
+      try {
+        const stat = await fs.stat(signtoolFilename);
+        if (stat.isFile()) {
+          fileName = signtoolFilename;
+          maxVersion = folderVersion;
+        }
+      } catch {
+        console.warn('Skipping %s due to error.', signtoolFilename);
+      }
+    }
+  }
+  if (fileName === '') {
+    throw new Error('Unable to find signtool.exe in ' + windowsKitsFolder);
+  }
+
+  console.log(`Signtool location is ${fileName}.`);
+  return fileName;
+};
+
+const generateExecutable = async (): Promise<void> => {
+  await fs.copyFile(process.execPath, EXECUTABLE_PATH_WIN as PathLike);
+};
+
+const removeSignature = async (): Promise<void> => {
+  const signtoolExePath = await getSigntoolLocation();
+  child.execSync(`${signtoolExePath} remove /s ${EXECUTABLE_PATH_WIN}`);
+};
+
+const injectExecutable = async (): Promise<void> => {
+  child.execSync(
+    `pnpx postject ${EXECUTABLE_PATH_WIN} NODE_SEA_BLOB ${BLOB_PATH} --sentinel-fuse ${NODE_SEA_FUSE}`
+  );
+};
+
+const packageRelease = async (): Promise<void> => {
+  console.log(`Generating windows executable`);
+  await generateExecutable();
+  console.log(`Removing signature`);
+  await removeSignature();
+  console.log(`Injecting code bundle into executable`);
+  await injectExecutable();
+};
+
+packageRelease()
+  .then(() => {
+    console.log(`Finished packaging windows release!`);
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error(`Error occurred while packaging windows release!`, e);
+    process.exit(1);
+  });
